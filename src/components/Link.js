7472c84e6d1ac977a4f48d6ed98b83082d6d9246
manuel blanco <manuel.22.lamas@gmail.com>
2018-05-13 16:44:13 -0400

    Fixed some typo errors on reducers logic

--- src/components/Link.js
+++ src/components/Link.js
@@ -8,7 +8,7 @@ const Link = ({ active, children, onClick }) => {
 
   return (
     <a href="#"
-       onClick={ e => {
+       onClick={e => {
         e.preventDefault();
         onClick();
       }}>
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -2,7 +2,6 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter;
-      break;
     default:
       return state
   };
